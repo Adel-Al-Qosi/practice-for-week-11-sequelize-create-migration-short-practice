92cf5eda7802b5de8943caac977fee6ca669e27a
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2024-05-17 01:04:51 +0300

    end practice

---
+++ server/db/migrations/20240516215040-create-color.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('Colors', {
+      name: Sequelize.DataTypes.STRING,
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primeryKey: true,
+        autoIncrement: true,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Colors');
+  }
+};
